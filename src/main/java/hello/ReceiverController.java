03574ebb4e66c5c5c6bfe7e4eb14c185b81889d2
faroukkhelil <39222112+faroukkhelil@users.noreply.github.com>
2018-10-14 13:14:38 +0100

    Add files via upload

---
+++ src/main/java/hello/ReceiverController.java
@@ -0,0 +1,24 @@
+package hello;
+
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+	public class ReceiverController {
+
+	    @RequestMapping("/")
+	    public String index() {
+	        return "Greetings from Spring Boot!  \n";
+	    }
+	    
+	    @RequestMapping(path="/customers", method= RequestMethod.GET)
+	    public long postCustomer(@RequestBody testArg customer){
+			return customer.getCustomerId();
+	    }
+
+	}
+
+
